13626183a4fce0c595ca5663f6e560593024d8c7
ahamad236 <ahmadshaik4u@gmail.com>
2025-09-29 16:53:34 +0530

    Added code in useFetch.js

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+    setLoading(true);
+
+    axios
+      .get(url)
+      .then((res) => {
+        if (isMounted) {
+          setData(res.data);
+          setLoading(false);
+        }
+      })
+      .catch((err) => {
+        if (isMounted) {
+          setError(err.message);
+          setLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
